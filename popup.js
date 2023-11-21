86d0eea1cf3b7e2dbfd68f15ba351b20dfb54db7
Stella Zhang <xinruzh@Stellas-MacBook-Air-8.local>
2023-11-20 22:40:45 -0500

    Add delete button

--- popup.js
+++ popup.js
@@ -10,6 +10,19 @@ document.addEventListener('DOMContentLoaded', function () {
               if (cookie.expirationDate) {
                 listItem.textContent += ", Expiration Date: " + new Date(cookie.expirationDate * 1000);
               }
+              
+              // display delete button
+              var deleteButton = document.createElement('button');
+              deleteButton.textContent = 'Delete';
+              deleteButton.onclick = function () {
+                chrome.cookies.remove({url: currentUrl, name: cookie.name}, function (deletedCookie) {
+                  console.log('Deleted Cookie:', deletedCookie);
+                  listItem.remove();
+                })
+              }
+
+              // add to display
+              listItem.appendChild(deleteButton);
               cookieList.appendChild(listItem);
           });
       });
